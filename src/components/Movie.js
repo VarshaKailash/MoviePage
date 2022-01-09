6f82a7c6b03f395135a97ebba6e3dedaef7aa7d8
VarshaKailash <Varsha7srinivasan@gmail.com>
2022-01-09 18:52:52 -0500

    Version1

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,8 @@ import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 //Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -15,15 +17,30 @@ import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
 
-const { movieId} = useParams();
-    const{ state: movie, loading, error} = useMovieFetch(movieId);
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
     if (loading) return <Spinner />;
     if (error) return <div>Something Went Wrong...</div>;
 
     return (
         <>
             <BreadCrumb movieTitle={movie.original_title} />;
-            <MovieInfo movie={movie} />     
+            <MovieInfo movie={movie} />
+            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                    key={actor.credit_id}
+                    name={actor.name}
+                    character={actor.character}
+                    imageUrl={
+                        actor.profile_path
+                        ?`${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                        : NoImage
+                    }
+                    />
+                ))}
+            </Grid>
         </>
     );
 };
---
+++ src/components/MovieInfoBar/index.js
@@ -0,0 +1,38 @@
+import react from "react";
+//Helpers
+import {calcTime, convertMoney} from '../../helpers';
+// Styles
+import { Wrapper } from "./MovieInfoBar.styles";
+import { Content } from "./MovieInfoBar.styles";
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+<Wrapper>
+    <Content>
+        <div className="column">
+            <p>
+                Running time: {calcTime
+                (time)}
+            </p>
+
+        </div>
+        <div className="column">
+            <p>
+                Budget: {convertMoney
+                (budget)}
+            </p>
+
+        </div>
+        <div className="column">
+            <p>
+                Revenue: {convertMoney
+                (revenue)}
+            </p>
+
+        </div>
+    </Content>
+</Wrapper>
+
+
+)
+
+export default MovieInfoBar;
